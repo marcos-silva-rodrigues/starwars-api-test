eeceab3e2e7b2f16db9c00d7eda43ce7bc2919d5
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-05-03 16:44:07 -0300

    chore: create project

---
+++ src/test/java/com/rodrigues/silva/marcos/starwarsapi/StarwarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigues.silva.marcos.starwarsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarwarsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
